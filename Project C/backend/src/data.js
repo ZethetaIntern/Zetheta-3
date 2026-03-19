29319a2c7431f1d02eac5e72e76d0d1bc8b77eb1
PrateekshaS231 <prateekshas.231@gmail.com>
2026-03-19 18:37:11 +0530

    Create data.js

---
+++ Project C/backend/src/data.js
@@ -0,0 +1,25 @@
+const assets = [
+  {
+    id: 1,
+    name: "Customer Portal",
+    exposure: 80,
+    security_level: 60,
+    value_if_compromised: 5000000
+  },
+  {
+    id: 2,
+    name: "Payment Gateway",
+    exposure: 70,
+    security_level: 75,
+    value_if_compromised: 10000000
+  },
+  {
+    id: 3,
+    name: "ATM Network",
+    exposure: 60,
+    security_level: 65,
+    value_if_compromised: 3000000
+  }
+];
+
+module.exports = assets;
